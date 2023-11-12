8d24a7f8d5589eabbcffa8025510e70695c838ed
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-12 11:51:28 +0100

    feat: supa types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,18 @@
+import type { Database } from '$lib/database.types';
 import type { Project } from '$lib/types';
 import { SupabaseClient, Session } from '@supabase/supabase-js';
 
 declare global {
-	type TProject = Project;
+	type TProject = Database['public']['Tables']['projects']['Row'];
+	type TTag = Database['public']['Tables']['tags']['Row'];
+
+	type DbResult<T> = T extends PromiseLike<infer U> ? U : never;
+	type DbResultOk<T> = T extends PromiseLike<{ data: infer U }> ? Exclude<U, null> : never;
+	type DbResultErr = PostgrestError;
 
 	namespace App {
 		interface Locals {
-			supabase: SupabaseClient;
+			supabase: SupabaseClient<Database>;
 			getSession(): Promise<Session | null>;
 		}
 		interface PageData {
