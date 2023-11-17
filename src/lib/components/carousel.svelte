fa64be60325a17f5d2ca2c800b074d5233161243
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-17 13:23:22 +0100

    feat: removed auto swipe on carousel

--- src/lib/components/carousel.svelte
+++ src/lib/components/carousel.svelte
@@ -4,45 +4,14 @@
 	import { onDestroy } from 'svelte';
 
 	export let numberOfItems: number;
-	export let delayBetweenSwipes: number = 5000;
 	export let currentIndex = 0;
-
-	let lastSwipeAt = new Date().getTime();
-	let swipeProgress = 0;
-	let swipeProgressElement: HTMLElement;
-	let hovering = false;
 
 	const next = () => (currentIndex = currentIndex + 1 > numberOfItems ? 0 : currentIndex + 1);
 
 	const previous = () => (currentIndex = currentIndex - 1 < 0 ? numberOfItems : currentIndex - 1);
-
-	const swipeInterval = setInterval(() => {
-		if (hovering) return;
-		next();
-		lastSwipeAt = new Date().getTime();
-	}, delayBetweenSwipes);
-
-	const progressInterval = setInterval(() => {
-		if (hovering) {
-			lastSwipeAt = new Date().getTime();
-			return;
-		}
-		swipeProgress = ((new Date().getTime() - lastSwipeAt) / delayBetweenSwipes) * 100;
-		swipeProgressElement.style.width = `${swipeProgress}%`;
-	}, 20);
-
-	onDestroy(() => {
-		clearInterval(swipeInterval);
-		clearInterval(progressInterval);
-	});
 </script>
 
-<div
-	class="h-full w-full flex items-center gap-3"
-	on:mouseenter={() => (hovering = true)}
-	on:mouseleave={() => (hovering = false)}
-	role="banner"
->
+<div class="h-full w-full flex items-center gap-3" role="banner">
 	<Button variant="ghost" on:click={previous}>
 		<ChevronLeft />
 	</Button>
@@ -50,10 +19,6 @@
 		{#key currentIndex}
 			<slot />
 		{/key}
-		<br />
-		<div class="w-4/5 mx-auto h-2 bg-primary/30 rounded-full">
-			<div class="h-full bg-primary rounded-full" bind:this={swipeProgressElement} />
-		</div>
 	</div>
 	<Button variant="ghost" on:click={next}>
 		<ChevronRight />
