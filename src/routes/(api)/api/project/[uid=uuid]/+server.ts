28083b673bd2f444991fdbb45717a16ccf775ffa
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-17 13:54:15 +0100

    added get endpoint

--- src/routes/(api)/api/project/[uid=uuid]/+server.ts
+++ src/routes/(api)/api/project/[uid=uuid]/+server.ts
@@ -15,3 +15,22 @@ export const DELETE: RequestHandler = async ({ params: { uid }, locals: { supaba
 
 	return new Response(null, { status: 204 });
 };
+
+export const GET: RequestHandler = async ({ params: { uid }, locals: { supabase } }) => {
+	const query = supabase
+		.from('projects')
+		.select('*')
+		.eq('uid', uid as string);
+
+	const { data, error }: DbResult<typeof query> = await query;
+
+	if (error) {
+		console.error(error);
+		return new Response(null, { status: 500 });
+	}
+
+	if (!data.length) return new Response(null, { status: 404 });
+
+	const project = data[0];
+	return new Response(JSON.stringify(project), { status: 200 });
+};
